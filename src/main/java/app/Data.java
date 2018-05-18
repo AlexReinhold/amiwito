d00c2a9377817b4b51a1185866d9f67e8b8e18df
Alex Reinhold <alexre86@gmail.com>
2018-05-18 15:21:35 -0400

    new data object

---
+++ src/main/java/app/Data.java
@@ -0,0 +1,44 @@
+package app;
+
+public class Data {
+
+    public String WHATSAPP = "Whatsapp";
+    public String NO_WHATSAPP = "NO Whatsapp";
+
+    private int id;
+    private String number;
+    private String type;
+
+    public Data(int id, String number) {
+        this.id = id;
+        this.number = number;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Data setId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public Data setNumber(String number) {
+        this.number = number;
+        return this;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public Data setType(String type) {
+        this.type = type;
+        return this;
+    }
+
+}
